de472928e00f660681cbbd6adbcee14442d992ef
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-04-23 11:24:14 -0500

    Navbar styled

---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+    return <div className="header">
+        <h1 className="content">Employee Finder</h1>
+        <p className="content">Use the search box to narrow the employee directory. You can also click on carrots to filter by name.</p>
+    </div>
+}
+
+export default Nav;
