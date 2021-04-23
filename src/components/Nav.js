6625d5694963e1ff6e2bbc76470cdf1e52c39d20
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-04-23 10:50:42 -0500

    Nav rendering w/o styling

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Nav() {
+    return <div class="header">
+        <h1>Employee Finder</h1>
+        <p>Use the search box to narrow the employee directory. You can also click on carrots to filter by name.</p>
+    </div>
+}
+
+export default Nav;
